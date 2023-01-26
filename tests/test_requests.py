023b3ef3724c4b0c9041bef6a94057b186de1c15
Artem <armlynskii@gmail.com>
2023-01-26 15:20:22 +0700

    initial commit

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -30,7 +30,7 @@ invalid_password = os.getenv('invalid_password')
 @allure.story('Данные пользователей')
 @allure.title('Отображение списка пользователей')
 def test_list_of_users():
-    with allure.step('GET запрос с параметром page == 2'):
+    with allure.step('GET запрос, отображает список пользователей на странице'):
         response = get_list_of_users(page={"page": 2})
     assert response.status_code == 200
     assert response.json()['page'] == 2
@@ -45,7 +45,8 @@ def test_list_of_users():
 @allure.story('Авторизация пользователя')
 @allure.title('Успешное создание пользователя')
 def test_create_user():
-    response = post_create_users(name, job)
+    with allure.step('POST запрос, создает пользователя'):
+        response = post_create_users(name, job)
     assert response.status_code == 201
     assert response.json()['name'] == name
     assert response.json()['job'] == job
@@ -62,7 +63,8 @@ def test_create_user():
 @allure.story('Авторизация пользователя')
 @allure.title('Успешное изменение данных пользователя')
 def test_update_user():
-    response = update_user(user_id, name, job)
+    with allure.step('PUT запрос, обновляет данные пользователя'):
+        response = update_user(user_id, name, job)
     assert response.status_code == 200
     assert response.json()['name'] == name
     assert response.json()['job'] == job
@@ -78,7 +80,8 @@ def test_update_user():
 @allure.story('Данные пользователей')
 @allure.title('Получение данных пользователя по id')
 def test_user_data():
-    response = get_user(user_id)
+    with allure.step('GET запрос, отображает данные пользователя '):
+        response = get_user(user_id)
     assert response.status_code == 200
     assert response.json() == S(get_user_schema)
     attach.add_curl(response)
@@ -91,7 +94,8 @@ def test_user_data():
 @allure.story('Регистрация')
 @allure.title('Регистрация пользователя')
 def test_successful_user_registration():
-    response = post_register_user(email, password)
+    with allure.step('POST запрос, успешная регистрация пользователя'):
+        response = post_register_user(email, password)
     assert response.status_code == 200
     assert response.json() == S(register_successful_user_shema)
     attach.add_body_request(response)
@@ -105,7 +109,8 @@ def test_successful_user_registration():
 @allure.story('Авторизация пользователя')
 @allure.title('Успешная авторизация пользователя')
 def test_login_successful():
-    response = post_login_user(email, password)
+    with allure.step('POST запрос, успешная авторизация пользователя'):
+        response = post_login_user(email, password)
     assert response.status_code == 200
     assert response.json() == S(login_successful_schema)
     attach.add_curl(response)
@@ -118,7 +123,8 @@ def test_login_successful():
 @allure.story('Авторизация пользователя')
 @allure.title('Неуспешная авторизация пользователя')
 def test_login_unsuccessful():
-    response = post_login_unsuccessful_user(email)
+    with allure.step('POST запрос, неуспешная авторизация пользователя'):
+        response = post_login_unsuccessful_user(email)
     assert response.status_code == 400
     assert response.json() == S(login_unsuccessful_schema)
     attach.add_body_request(response)
@@ -132,7 +138,8 @@ def test_login_unsuccessful():
 @allure.story('Авторизация пользователя')
 @allure.title('Неуспешная регистрация пользователя')
 def test_unsuccessful_user_registration():
-    response = post_unsuccessful_register_user(invalid_email, invalid_password)
+    with allure.step('POST запрос, неуспешная регистрация пользователя'):
+        response = post_unsuccessful_register_user(invalid_email, invalid_password)
     print(response)
     assert response.status_code == 400
     assert response.json() == S(register_unsuccessful_user_schema)
